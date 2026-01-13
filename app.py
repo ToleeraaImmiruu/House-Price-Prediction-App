6fecc19e620e9f72ad2caf90f05d1cb210902e8b
Tolera Imiru <toleeraa2024@gmail.com>
2026-01-13 08:22:19 +0300

    adding the backemd to deploy the model
    Added an import statement for numpy.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import joblib
 import numpy as  np
 import pandas as pd
+
 # Load your trained models
 model = joblib.load("model.pkl")
 #App title when the page was loading to the user of the data
