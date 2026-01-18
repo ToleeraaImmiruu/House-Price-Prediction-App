efd8f121bcb68443d693f7c4eae298c0f11591d2
Tolera Imiru <toleeraa2024@gmail.com>
2026-01-18 21:00:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ bedrooms = st.number_input("Number of Bedrooms", min_value=0, value=3)
 bathrooms = st.number_input("Number of Bathrooms", min_value=0, value=2)
 condition = st.number_input("Condition (1–5)", min_value=1, max_value=5, value=3)
 number_of_schools = st.number_input("Number of Schools Nearby", min_value=0, value=2)
-
 st.divider() #this was used to add the break line to the pages 
 # Prepare input from the user 
 
