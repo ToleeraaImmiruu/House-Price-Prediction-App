fa688abdfa37afbf8e96f48066022a65fbfa5a67
Tolera Imiru <toleeraa2024@gmail.com>
2026-04-24 10:45:16 +0300

    Fix formatting and improve input field descriptions

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ st.divider()
 st.write("This app uses a trained Machine Learning model to predict house prices based on input features the make the prediction . Enter the details below and click **Predict** to get an estimated price."
 st.divider() 
 # Input fields
-
 bedrooms = st.number_input("Number of Bedrooms", min_value=0, value=3)
 bathrooms = st.number_input("Number of Bathrooms", min_value=0, value=2)
 condition = st.number_input("Condition (1–5)", min_value=1, max_value=5, value=3)
